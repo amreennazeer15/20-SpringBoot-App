f3be261015a0a91c4769ea0179c43c8ce94aaddb
Amreen <amreennazeer87@gmail.com>
2021-05-28 17:15:07 +0530

    HIS-300 CHANGES

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -6,7 +6,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
+	
+	private static final String REST_URI = "https://www.equifax.com/getScores";
+	
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 		//changes for HIS-134
